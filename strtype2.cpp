823788d215e6bcd1c6c1486e5c302cabc5500da4
katherinnedmitrieva-lang <katherinnedmitrieva@gmail.com>
2026-04-16 22:21:14 +0300

    Ввід строки з пробілами

---
+++ strtype2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
+int main() {
+    char charr[20];
+    string str;
+
+    cout<<"Lenght of string in char before input: "<<strlen(charr)<<endl;
+    cout<<"Lenght of string in string before input: "<<str.size()<<endl;
+    cout<<"Enter a line of text: ";
+    cin.getline(charr, 20);
+    cout<<"You entered: "<<charr<<endl;
+    cout<<"Enter another line of text: ";
+    getline(cin, str);
+    cout<<"You entered: "<<str<<endl;
+    cout<<"Lenght of string in char after input: "<<strlen(charr)<<endl;
+    cout<<"Lenght of string in string after input: "<<str.size()<<endl;
+
+    return 0;
+}
